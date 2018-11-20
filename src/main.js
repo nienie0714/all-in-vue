0cf42c355fb2456b71c49fc1495d3394576a6ade
nienie0714 <niexinyu0714@163.com>
2018-11-20 14:25:02 +0800

    add websocket ws

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import store from './store';
+import axios from 'axios';
+import ElementUI from 'element-ui';
+// import 'element-ui/lib/theme-chalk/index.css'; // 默认主题
+import '../static/css/theme-green/index.css'; // 浅绿色主题
+import '../static/css/icon.css';
+import 'babel-polyfill';
 
-Vue.config.productionTip = false
+Vue.use(ElementUI, { size: 'small' });
+Vue.prototype.$axios = axios;
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app');
